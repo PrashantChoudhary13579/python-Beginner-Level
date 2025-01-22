fe6867a0579a3bef92537935b76e384d8d29170b
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-01-22 23:10:07 +0530

    Getting some theoratical knowledge of the oops

---
+++ Day1/oops1.py
@@ -0,0 +1,33 @@
+# OBject ?
+# Everything is object in the python, for us variable and object are same. 
+# It is the instance of the class
+
+
+# OOP(Object Oriented Programing )-
+# 1. Encapsulation
+# 2. Object
+# 3. Inheritance
+# 4. Polymorphism
+# 5. Abstraction
+
+a = 5
+print(type(a))
+
+L = [1,2,3,4]
+print(type(L))
+
+# Class - It is a blueprint - of how an object will behave 
+# There are two main things -  data and it's function/Behavior
+'''
+Pascal case -   ThisIsPascalCase ( first letter of every word is capital and joined)
+Camel case - thisIsCamelCase ( first letter of every word is capital and joined except the first one)
+Snake Case - this_is_snake_case (all letter are joined together through underscore)
+
+class Car:  
+    color = "Blue" #data
+    model = "sports"
+
+    def calculate_avg_speed(km, time):
+    # some code
+
+''' 
