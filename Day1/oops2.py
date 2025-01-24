75ba594bf84cd8aacea465c268bad3bcc145a3b7
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-01-24 14:33:31 +0530

    Containing some information about oops

---
+++ Day1/oops2.py
@@ -0,0 +1,17 @@
+# constructor - ismein hum vo code likhte h jo apne aap start ho jaye background mein jisko hum user ko nahi dena chate
+# self - jis bhi object ke sath aap abhi/currently kaam kr rahe ho vo hi self h 
+
+# Creating a data type 
+x = 5/6
+print(x)
+
+# class mein do chij hoti h - data and uske function 
+# If we want to access them it will be only possible if we make the object of the class 
+# While accessing any of the data or the function that work can be easily done with the help of the object of the class as
+# By using the dot operator - sbi.deposit(), sbi.withdraw() , sbi.amount(), hdfc.deposit(), hdfc.withdraw(), hdfc.exitint()
+
+# This is done for basically 2 object if we have multiple objects then we will have a reference variable which changes it's object/value each time.
+
+# Here comes *self* as that reference variable.
+
+# self - this is the current object 
