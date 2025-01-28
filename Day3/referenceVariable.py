6b7e00e0e7ea69f7b73e0c3bf2ce78d662f546ed
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-01-29 00:01:13 +0530

    Learning about the reference variable

---
+++ Day3/referenceVariable.py
@@ -0,0 +1,109 @@
+# reference variable
+
+
+class Atm:
+    def __init__(self):
+        self.__pin = ""
+        self.__balance = 0
+        self.menu()
+
+    def get_pin(self):
+        return self.__pin
+
+    def set_pin(self, new_pin):
+        if type(new_pin) == int:
+            self.__pin = new_pin
+            print("Pin change")
+        else:
+            print("Not allowed")
+
+    def menu(self):
+
+        choice = int(input ("""Hello , How would you like to proceed ?
+               1. Enter 1 to create pin
+               2. Enter 2 to deposit 
+               3. Enter 3 to withdraw
+               4. Enter 4 to check balance
+               5. Enter 5 to exit \n"""))
+        
+        if(choice == 1):
+            self.create_pin()
+        elif(choice == 2):
+            self.deposit()
+        elif(choice == 3):
+            self.withdraw()
+        elif(choice == 4):
+            self.check_balance()
+        elif(choice == 5):
+            self.exiting()
+        else:
+            print("Invalid choice , please try again")
+            # self.menu()
+
+    def create_pin(self):
+        self.__pin = int(input("Enter your pin"))
+        print("Pin created successfully")
+        
+
+    def deposit(self):
+        self.temp = int(input("Enter your pin"))
+        if (self.temp == self.__pin):
+            amount = int(input("Enter deposit amount"))
+            self.__balance += amount
+            print("Amount deposited Successfully ")
+        else:
+            print("Invalid pin")
+        
+    
+    def withdraw(self):
+        self.temp = int(input("Enter your pin"))
+        if(self.temp == self.__pin):
+            amount = int (input("Enter withdraw amount"))
+            self.__balance -= amount
+            print("Amount withdraw Successfully")
+        else:
+            print("Invalid pin")
+        
+    
+    def check_balance(self):
+        self.temp = int(input("Enter your pin"))
+        if(self.temp == self.__pin):
+            print(f"Balance amouont : {self.__balance}")
+        else:
+            print("Invalid pin")
+        
+    
+    def exiting(self):
+        print("Exiting the system")
+        exit()
+
+
+sbi = Atm()
+
+
+"""
+
+Atm()
+
+Hello , How would you like to proceed ?
+               1. Enter 1 to create pin
+               2. Enter 2 to deposit 
+               3. Enter 3 to withdraw
+               4. Enter 4 to check balance
+               5. Enter 5 to exit 
+
+1
+
+Enter your pin12345
+Pin created successfully
+
+<__main__.Atm object at 0x00000171B186CDD0>
+
+# In this the object is stored at any random way not known to us but we are only having it's address
+ 
+sbi = Atm()
+
+# But by doing this we stored the object in a variable (sbi) known as reference variable
+
+"""
+
