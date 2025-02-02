0a30aa54b4c6ae713c287fdb5d530fd1aeaa8208
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-02-02 23:57:03 +0530

    List - operations

---
+++ Baasics/list.py
@@ -0,0 +1,45 @@
+# What is a List 
+# It is a type of datatype where we can store mulitple things 
+
+# List vs Array - fundamentally they are same
+# 1.Array is homogeneous - same datatype 
+# 1.List can be hetrogeneous - diff. datatype
+# 2.In array, Data are stored in a continuous memory location , while it is not in list.
+# 3.Array are faster then list
+# 4.List are more programmer friendly
+# 5.List are dynamic in nature, while array are static in nature.
+
+# Create
+l = []
+print(l)
+l = [1,2,3,4,5]
+print(l)
+
+l2 = ["Hello", 4,5,6,True,5+7j]
+print(l2)
+
+# Multi-dim lists
+# 2D list
+l3 = [1,2,3,[4,5,6]]
+print(l3)
+# example of heterogeneous
+
+# 3D list
+l4 = [[[1,2],[3,4]],[[5,6],[7,8]]]
+print(l4)
+
+l5 = list("Halldia")
+print(l5)
+
+l6 = list()
+print(l6)
+
+print(l2[4])
+print(l2[5])
+# Access
+# Edit
+# Add 
+# Delete
+# operations
+# Functions
+#  
