569f3ed9a54e39255e49a7e1fe9975a69fde512e
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-05-29 22:52:39 +0530

    Revising the basics of python

---
+++ PythonRevise/code1.py
@@ -0,0 +1,28 @@
+print("Hello Prashant, Revising python again")
+name = "Prashant Choudhary"
+age = 22.4
+is_adult = True
+print(name, age, is_adult)
+
+# Exercise 1 
+person = "Tony Stark"
+age = "51"
+title = "Genius"
+print ("Do you know",person,"?")
+print ("Yes, he is",age,"years old and he is a ",title)
+
+# Taking input from user
+# name = input("Enter your name ")
+# print("Hello "+ name)
+
+# Exercise 2 
+# superhero = input("Hi Tony, What is your secret superhero name ? ")
+# print("Yes, I found it. His name is ",superhero)
+
+# Type conversion
+old_age  = int(input("Enter your old age "))
+new_age = old_age + 2
+print(new_age)
+
+number = 18
+print(float(18))
